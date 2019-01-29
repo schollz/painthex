9576ad9779042e55256de832303dae53d789194e
Zack Scholl <zack.scholl@gmail.com>
2019-01-29 11:26:23 -0800

    extract hex code

--- blick/cut.go
+++ blick/cut.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	"image"
+	"image/color"
+	"image/draw"
 	"image/jpeg"
 	"image/png"
 	"log"
 	"os"
 	"path/filepath"
 
+	"github.com/go-playground/colors"
 	"github.com/oliamb/cutter"
 )
 
@@ -17,7 +20,11 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, inPath := range jpgFiles {
+	for i, inPath := range jpgFiles {
+		fmt.Println(inPath)
+		if i > 0 {
+			break
+		}
 		err = crop(inPath, inPath+".1.png", image.Point{60, 300})
 		if err != nil {
 			log.Fatal(err)
@@ -39,7 +46,6 @@ func crop(inPath string, outPath string, tlPoint image.Point) (err error) {
 		return
 	}
 	defer fi.Close()
-	fmt.Println(fi)
 	img, err := jpeg.Decode(fi)
 	if err != nil {
 		return
@@ -65,5 +71,46 @@ func crop(inPath string, outPath string, tlPoint image.Point) (err error) {
 	defer fo.Close()
 
 	err = png.Encode(fo, cImg)
+	cnrgba, err := AverageImageColor(outPath)
+	rgb, err := colors.RGB(cnrgba.R, cnrgba.G, cnrgba.B)
+	if err != nil {
+		return
+	}
+	fmt.Println(rgb.ToHEX().String())
 	return
+}
+
+func AverageImageColor(inPath string) (cnrgba color.NRGBA, err error) {
+	fi, err := os.Open(inPath)
+	if err != nil {
+		return
+	}
+	defer fi.Close()
+	im, err := png.Decode(fi)
+	if err != nil {
+		return
+	}
+	rgba := imageToRGBA(im)
+	size := rgba.Bounds().Size()
+	w, h := size.X, size.Y
+	var r, g, b int
+	for y := 0; y < h; y++ {
+		for x := 0; x < w; x++ {
+			c := rgba.RGBAAt(x, y)
+			r += int(c.R)
+			g += int(c.G)
+			b += int(c.B)
+		}
+	}
+	r /= w * h
+	g /= w * h
+	b /= w * h
+	cnrgba = color.NRGBA{uint8(r), uint8(g), uint8(b), 255}
+	return
+}
+
+func imageToRGBA(src image.Image) *image.RGBA {
+	dst := image.NewRGBA(src.Bounds())
+	draw.Draw(dst, dst.Rect, src, image.ZP, draw.Src)
+	return dst
 }
